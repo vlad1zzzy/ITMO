ebc02ad83e4db0a91709a7831898192482021e5a
Kryukov Vlad <62171938+vlad1zzzy@users.noreply.github.com>
2020-09-28 18:20:13 +0300

    Update 1.sh

--- 2 course/OC/lab1/1.sh
+++ 2 course/OC/lab1/1.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 max=$1
-if [[ $2 -gr $1 ]]; then max=$2; fi;
-if [[ %3 -gr $max ]]; then max=$3; fi;
+if [[ $2 -gt $1 ]]; then max=$2; fi;
+if [[ %3 -gt $max ]]; then max=$3; fi;
 echo $max;
 
