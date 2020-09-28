1db5304165c606d3beb0f37a353f9d984e0c6f99
Kryukov Vlad <62171938+vlad1zzzy@users.noreply.github.com>
2020-09-28 13:06:25 +0300

    Update 10.sh

--- 2 course/OC/lab1/10.sh
+++ 2 course/OC/lab1/10.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-man bash | grep -owh '\w\{4,\}' | tr "[:upper:]" "[:lower:]" | sort | uniq -c | sort -r -n | head -3 | awk '{print $2}'
+man bash | grep -h -s -o -w '\w\{4,\}' | tr "[:upper:]" "[:lower:]" | sort | uniq -c | sort -r -n | head -3 | awk '{print $2}'
